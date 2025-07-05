37739c44b3fb5d248dcaee1aa23ef0cf3edf5862
Ukaegbu Gray Nneji <84023651+graynneji@users.noreply.github.com>
2025-07-05 04:06:13 -0700

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://djfomvrcddgktaafqgwm.supabase.co";
+export const supabaseUrl = "https://nmqbckwbbtwibyekyehe.supabase.co";
+// export const supabaseUrl = "https://djfomvrcddgktaafqgwm.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZm9tdnJjZGRna3RhYWZxZ3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExOTU5NzIsImV4cCI6MjAyNjc3MTk3Mn0.p4Re8J_0h47QwPCxtd3gjxb1NF2pDkxoJOR7oiGXCxY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tcWJja3diYnR3aWJ5ZWt5ZWhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2NDczNDQsImV4cCI6MjA2NzIyMzM0NH0.avnXnQ8d1M3dLrtGDq8CiwzXKCgEadVLaYPNj-3E1Hs";
+  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZm9tdnJjZGRna3RhYWZxZ3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExOTU5NzIsImV4cCI6MjAyNjc3MTk3Mn0.p4Re8J_0h47QwPCxtd3gjxb1NF2pDkxoJOR7oiGXCxY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
